3505b9cdb27f81bf0d6fc403d539e6c64c637c59
Oleh Hrynets <oleg.grinets@gmail.com>
2023-06-20 22:25:36 -0400

    I changed engine to model

--- langchain_test.py
+++ langchain_test.py
@@ -13,7 +13,7 @@ with open('env.json') as json_file:
     data = json.load(json_file)
     openai_api_key = data['openai_api_key']
     temperature = data['openai_temperature']
-    engine = data['openai_engine']
+    model = data['openai_model']
 
 # A simple langchain skeleton with OpenAI configuration
 # This is a simple skeleton for a language chain
@@ -21,8 +21,6 @@ with open('env.json') as json_file:
 llm = OpenAI(openai_api_key = openai_api_key, temperature = temperature )
 result = llm.predict("What would be a good company name for a company that makes colorful socks?")
 print(result)
-
-
 
 chat = ChatOpenAI(openai_api_key = openai_api_key, temperature=temperature)
 result =  chat.predict_messages([HumanMessage(content="Translate this sentence from English to French. I love programming.")])
