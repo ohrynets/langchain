ab2de3da6169c222c1ccbc5f4f1d1de30c2d90b6
Oleg Grynets <Oleg_Grynets@epam.com>
2023-06-22 19:51:29 -0400

    Added token usage statistics

--- langchain_test.py
+++ langchain_test.py
@@ -2,6 +2,8 @@
 import json
 from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
+from langchain.callbacks import get_openai_callback
+
 from langchain.schema import (
     AIMessage,
     HumanMessage,
@@ -18,10 +20,14 @@ with open('env.json') as json_file:
 # A simple langchain skeleton with OpenAI configuration
 # This is a simple skeleton for a language chain
 
-llm = OpenAI(openai_api_key = openai_api_key, temperature = temperature )
-result = llm.predict("What would be a good company name for a company that makes colorful socks?")
-print(result)
+with get_openai_callback() as cb:
+    llm = OpenAI(openai_api_key = openai_api_key, temperature = temperature )
+    result = llm.predict("What would be a good company name for a company that makes colorful socks?")
+    print(result)
+    print(cb)
 
-chat = ChatOpenAI(openai_api_key = openai_api_key, temperature=temperature)
-result =  chat.predict_messages([HumanMessage(content="Translate this sentence from English to French. I love programming.")])
-print(result)
+with get_openai_callback() as cb:
+    chat = ChatOpenAI(openai_api_key = openai_api_key, temperature=temperature)
+    result =  chat.predict_messages([HumanMessage(content="Translate this sentence from English to French. I love programming.")])
+    print(result)
+    print(cb)
